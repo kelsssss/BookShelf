57101e28e375ee263941fe82e6ee469f59370975
zakhar <zaxarbelov@yandex.com>
2024-11-07 11:45:50 +0300

    update

--- app/src/main/java/com/example/bookshelf/ui/screens/ErrorScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/ErrorScreen.kt
@@ -6,14 +6,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bookshelf.R
 import com.example.bookshelf.ui.theme.BookShelfTheme
 
@@ -35,13 +42,17 @@ fun ErrorScreen(
                 painter = painterResource(R.drawable.error),
                 contentDescription = "Error occurred",
                 modifier = modifier
-                    .fillMaxWidth(0.7f)
+                    .fillMaxWidth(0.5f)
             )
             Text(
-                text = "An error occurred, check your internet connection and try again later"
+                text = "An error occurred\nCheck your internet connection and try again later.",
+                textAlign = TextAlign.Center,
+                modifier = modifier
+                    .padding(top = 10.dp, bottom = 10.dp)
             )
             Button(
-                onClick = { }
+                onClick = { },
+//                colors = ButtonDefaults.buttonColors(containerColor = Color.)
             ) {
                 Text(
                     "Try again"
--- app/src/main/java/com/example/bookshelf/ui/screens/LoadingScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/LoadingScreen.kt
@@ -1,2 +1,55 @@
 package com.example.bookshelf.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bookshelf.R
+import com.example.bookshelf.ui.theme.BookShelfTheme
+
+@Composable
+fun LoadingScreen(
+    modifier : Modifier = Modifier
+){
+    Surface(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.loading),
+                contentDescription = "Error occurred",
+                modifier = modifier
+                    .fillMaxWidth(0.4f)
+            )
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingScreenPreview(){
+    BookShelfTheme() {
+        LoadingScreen()
+    }
+}
