fbfd958e880da3c4a6446e5840da20ae84b66114
zakhar <zaxarbelov@yandex.com>
2024-11-15 14:29:02 +0300

    some bugs fixed

--- app/src/main/java/com/example/bookshelf/ui/screens/BookCard.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/BookCard.kt
@@ -14,10 +14,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil3.compose.AsyncImage
+import com.example.bookshelf.R
 import com.example.bookshelf.ui.BooksViewModel
 import kotlinx.coroutines.launch
 
@@ -52,6 +54,7 @@ fun BookCard(
                 model = bookImgUrl,
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
+                error = painterResource(R.drawable.error),
                 modifier = modifier
                     .fillMaxSize()
                     .weight(8f),
